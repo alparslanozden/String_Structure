1c9ddcd7cdeecc539f1364cee4bca4f846ac2ef3
alparslanozden <ahmetalparslanozden@gmail.com>
2022-04-30 23:53:29 +0300

    Code Refactor - String Methods

---
+++ app/src/main/java/com/example/string_structure/StringMetodlar.kt
@@ -0,0 +1,10 @@
+package com.example.string_structure
+
+fun main(){
+
+    val str1 = "Merhaba"
+    println(str1.subSequence(0,3))                  // M-0, E-1, R-2, H-3       İLK 3 HARFİ ALIR
+
+
+
+}
